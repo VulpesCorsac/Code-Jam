553596868e68a14abac7f005b74b97a6c6726b12
George <shaxirrim@gmail.com>
2020-04-29 10:54:29 +0300

    1B preparation and Qualification renaming

---
+++ 2020/1B/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+int main() {
+//    freopen("input.txt", "r+", stdin);
+//    freopen("output.txt", "w+", stdout);
+
+    return 0;
+}
